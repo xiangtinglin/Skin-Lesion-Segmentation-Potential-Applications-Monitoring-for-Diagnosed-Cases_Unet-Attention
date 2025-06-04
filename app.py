5b3cdf881189e5f5ed583b85e00b79d06915f957
林湘婷 <116503010+xiangtinglin@users.noreply.github.com>
2025-06-04 14:03:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ def load_model():
     model_path = "./checkpoint/unet_stat_attention_best.pth"
     if not os.path.exists(model_path):
         os.makedirs("checkpoint", exist_ok=True)
-        file_url = "https://drive.google.com/uc?id=1KAOUlpDmTb-ePfB1GTFo_Rln136cZwQR"
-        output = gdown.download(file_url, model_path, quiet=False)
+        url = "https://drive.google.com/uc?id=1KAOUlpDmTb-ePfB1GTFo_Rln136cZwQR"
+        output = gdown.download(url, model_path, quiet=False)
         if output is None or not os.path.exists(model_path):
-            raise FileNotFoundError("Model download failed! Check Google Drive link or ID.")
+            raise FileNotFoundError("Model download failed! Please verify the Google Drive link or ID.")
 
     model = UNet_EdgeBranch_AttentionGate()
-    device = torch.device("cpu")  # 強制使用 CPU 避免 Streamlit Cloud 出錯
+    device = torch.device("cpu")
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     model.eval()
